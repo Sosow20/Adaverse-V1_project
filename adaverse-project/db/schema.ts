614226a7267f435b793309f9e84eb454bfd7d88a
Sosow20 <sofiacherifi1995@gmail.com>
2025-11-24 11:22:41 +0100

    second commit

---
+++ adaverse-project/db/schema.ts
@@ -0,0 +1,13 @@
+import { pgTable, serial, text, integer, pgEnum } from 'drizzle-orm/pg-core';
+
+
+export const adaProjects = pgTable('adaProjects', {
+});
+
+
+export const prommotions = pgTable('prommotions', {
+
+});
+
+export const studentProjects = pgTable('studentProjects', {
+});
